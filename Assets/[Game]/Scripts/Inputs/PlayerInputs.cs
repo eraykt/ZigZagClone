f96b0fc27bc2e428610598aedf391d0007dc8495
eray <alieraykt@gmail.com>
2022-12-12 01:57:54 +0300

    salak problemi çözdüm

--- Assets/[Game]/Scripts/Inputs/PlayerInputs.cs
+++ Assets/[Game]/Scripts/Inputs/PlayerInputs.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public static class PlayerInputs
 {
     public static bool LeftClick => Input.GetMouseButtonDown(0);
 
-    public static bool ChangeDirection => LeftClick;
+    public static bool ChangeDirection => !EventSystem.current.IsPointerOverGameObject(0) && LeftClick;
 }
