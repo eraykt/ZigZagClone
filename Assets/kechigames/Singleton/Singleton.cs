ecb394c1100ac01bcc3128c2c84bddc3bd98952b
eray <alieraykt@gmail.com>
2022-12-12 18:23:07 +0300

    Update Singleton.cs

--- Assets/kechigames/Singleton/Singleton.cs
+++ Assets/kechigames/Singleton/Singleton.cs
@@ -6,7 +6,7 @@ namespace kechigames.Singleton
     {
         public static T Instance { get; private set; }
 
-        private void Awake()
+        protected virtual void Awake()
         {
             if (Instance != null)
             {
